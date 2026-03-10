4f9742c1092e7383c2e9a61e0505e26cf1d40ced
Linkaform Develop Team <backend@linkaform.com>
2026-03-10 13:50:36 -0600

    main creafuncion

---
+++ funcion.py
@@ -0,0 +1,47 @@
+
+
+
+
+class test():
+
+
+    def partial_return(articulo, data_concesion):
+        equipos = data_concesion['equipos']
+        data = {
+            'record_id' : articulo['id'],
+            'status' : 'partial',
+            'quien_entrega' :  data_concesion['persona_nombre_otro'],
+            'identificacion_entrega' : data_concesion['persona_identificacion_otro'],
+            'comentarios' : "Devolucion de Parical: partial_return",
+            'entregado_por': "otro",
+            'equipos' : []
+            }
+        for equipo in equipos:
+            row = {}
+            row['id_movimiento'] = equipo['id_movimiento']
+            row['cantidad_devuelta'] = round(equipo['cantidad_equipo_concesion']/3)
+            row['state'] = "complete"
+            row['evidencia'] = [{
+                'file_name':'equipo_total.png',
+                'file_url':'https://f001.backblazeb2.com/file/app-linkaform/public-client-126/68600/6076166dfd84fa7ea446b917/2026-02-25T11:44:39_5.png'}]
+            data['equipos'].append(row)
+        response = acceso_obj.update_article_concessioned(data, articulo['id'])
+        return True
+
+    def complete_return_empleado(articulo, data_concesion):
+        data = {
+            'record_id' : articulo['id'],
+            'status' : 'total',
+            'state' : 'complete',
+            'quien_entrega' :  data_concesion['persona_nombre_concesion'],
+            'identificacion_entrega' : data_concesion['persona_identificacion_otro'],
+            'comentarios' : "Devolucion de Pruebas: complete_return_empleado",
+            'evidencia' : [{
+                'file_name':'equipo_total.png',
+                'file_url':'https://f001.backblazeb2.com/file/app-linkaform/public-client-126/68600/6076166dfd84fa7ea446b917/2026-02-25T11:44:39_5.png'}]
+            }
+        response = acceso_obj.update_article_concessioned(data, articulo['id'])
+        return response
+
+    def one_article_return():
+        return True
